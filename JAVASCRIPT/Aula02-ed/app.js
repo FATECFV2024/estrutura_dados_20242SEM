66ae430b6f2981983cfe1f0e5c11053a3c18ffc0
Francisco Douglas <98854868+franciscodouglas@users.noreply.github.com>
2024-09-13 10:10:38 -0300

    Update app.js

--- JAVASCRIPT/Aula02-ed/app.js
+++ JAVASCRIPT/Aula02-ed/app.js
@@ -51,3 +51,61 @@ console.log(existe)
 // Unificando como texto puro
 let texto = frutas.join(',')
 console.log(texto)
+
+
+let numeros = [1,2,3,4,5,6]
+let dobra = numeros.map(x=> x*2)
+
+console.log(dobra)
+
+//exemplo de filter
+
+let pares = numeros.filter(x=> x % 2 ==0)
+
+console.log(pares)
+
+let localiza_elemento = numeros.find(x=> x>2)
+let localiza_index = numeros.findIndex(x=> x>2)
+
+console.log("Foi localizado o elemento: "+localiza_elemento)
+console.log("Foi localizado o index: "+localiza_index)
+
+
+let maior = numeros.some(x=> x>2)
+
+console.log("Atende a condição lógica: "+maior)
+
+let r =  numeros.every(x=> x>0)
+console.log("Todos atendem a regra: "+r)
+
+// organizar os elementos
+let n_aleatorio = [05,03,01,0,06,02,08,07]
+console.log(n_aleatorio)
+n_aleatorio.sort()
+console.log(n_aleatorio)
+
+n_aleatorio.reverse()
+console.log(n_aleatorio)
+
+
+
+
+
+//ternario (if-else simplificado em uma linha)
+let nota = 6
+let res = nota>=6  ? "Aprovado":"Reprovado"
+
+console.log(res)
+//ou
+if (nota>=6){
+    console.log("Aprovado")
+}else{
+    console.log("Reprovado")
+}
+
+// exemplo de funcao
+function teste(t){
+    console.log(t)
+}
+
+// teste('Francisco')
